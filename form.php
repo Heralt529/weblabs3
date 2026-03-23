269b45991942335021a0ca7857d4b7966d18b4b5
Heralt529 <inog10@yandex.ru>
2026-03-23 10:48:02 +0300

    Update form.php

--- form.php
+++ form.php
@@ -1,17 +1,17 @@
 <form action="" method="POST">
-  <input name="fio" maxlength="128" placeholder="фио"/>
-  <input name="phone" type="tel" maxlength="11" placeholder="+7-777-777-77-77"/>
-  <input name="email" type="email" maxlength="128" placeholder="email@gmail.com"/>
-  <select name="year"/>
+  <input name="name" />
+  <!-- <input name="number"/>
+  <input name="email"/> -->
+  <select name="year">
     <?php 
     for ($i = 1922; $i <= 2022; $i++) {
       printf('<option value="%d">%d год</option>', $i, $i);
     }
     ?>
   </select>
+  <!-- <input name="sex"/>
+  <input name="language"/>
+  <input name="biography"/> -->
 
-  <input type = "radio" id = "sex" name='sex"/>
-    <label for="sex"
-  
   <input type="submit" value="ok" />
 </form>
